a69ce65ae62d5dd1b44552fe3e6f903a09c26ca5
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-05-11 19:06:17 +0600

    finish sidenav upper part

--- src/Components/Pages/SideNav/SideNav.jsx
+++ src/Components/Pages/SideNav/SideNav.jsx
@@ -1,29 +1,27 @@
-import { icons } from "lucide-react";
+import { PencilRuler, Image, ShieldPlus } from "lucide-react";
+import React from "react";
 
 const SideNav = () => {
-
     const menuList = [
-        { id: 1, name: 'Icon', icon: 'PencilRuler' },
-        { id: 2, name: 'Background', icon: 'image' },
-        { id: 3, name: 'Upgrade', icon: 'shield-plus' },
-    ]
+        { id: 1, name: 'Icon', icon: <PencilRuler /> },
+        { id: 2, name: 'Background', icon: <Image /> },
+        { id: 3, name: 'Upgrade', icon: <ShieldPlus /> },
+    ];
 
     return (
-        <div>
-
+        <div className="border h-[100vh]">
             <div>
-                {
-                    menuList.map((menu) => {
-                        return (
+                {menuList.map((menu) => (
+                    <div key={menu.id} className="flex items-center gap-2 w-full hover:bg-primary hover:text-white hover:rounded-md transition-all duration-200 ease-in-out">
 
-                            <div key={menu.id}>
-                                <h2 className="p-3 text-lg px-7 text-gray-700 my-2 cursor-pointer hover:bg-primary hover:text-white hover:rounded-md transition-all duration-200 ease-in-out">{menu.name}</h2>
-                            </div>
-                        )
-                    })
-                }
+                        <div className="ml-5">
+                            {menu.icon}
+                        </div>
+
+                        <h2 className="p-3 text-lg px-7 text-gray-700 my-2 cursor-pointer hover:text-white">{menu.name}</h2>
+                    </div>
+                ))}
             </div>
-
         </div>
     );
 };
