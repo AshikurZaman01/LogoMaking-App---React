edeec04bb1cb4b7f3b09125a04c33334dc6425a1
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-05-11 17:48:42 +0600

    add header

--- src/Components/Pages/Header/Header.jsx
+++ src/Components/Pages/Header/Header.jsx
@@ -1,7 +1,19 @@
+import { Download, DownloadCloud, DownloadCloudIcon } from "lucide-react";
 
 const Header = () => {
     return (
-        <div>
+        <div className="container mx-auto my-2 border-b pb-2">
+
+            <div className="flex justify-between">
+                <div>
+                    <h1 className="text-4xl font-bold cursor-pointer">Logo</h1>
+                </div>
+
+                <div>
+                    <button className="btn  btn-error text-white flex gap-2 items-center"><DownloadCloudIcon></DownloadCloudIcon> Download</button>
+                </div>
+            </div>
+
         </div>
     );
 };
