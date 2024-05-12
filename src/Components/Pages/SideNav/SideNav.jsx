bec29b7aa322c3cf14ce5b14bde6e5f1ec7cca16
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-05-12 14:34:16 +0600

    add onclick and make active

--- src/Components/Pages/SideNav/SideNav.jsx
+++ src/Components/Pages/SideNav/SideNav.jsx
@@ -1,18 +1,22 @@
 import { PencilRuler, Image, ShieldPlus } from "lucide-react";
-import React from "react";
+import React, { useState } from "react";
 
 const SideNav = () => {
+
     const menuList = [
         { id: 1, name: 'Icon', icon: <PencilRuler /> },
         { id: 2, name: 'Background', icon: <Image /> },
         { id: 3, name: 'Upgrade', icon: <ShieldPlus /> },
     ];
+    const [activeIndex, setActiveIndex] = useState(0);
 
     return (
         <div className="border h-[100vh]">
             <div>
-                {menuList.map((menu) => (
-                    <div key={menu.id} className="group flex items-center gap-2 w-full hover:bg-primary hover:text-white hover:rounded-md transition-all duration-200 ease-in-out hover:cursor-pointer">
+                {menuList.map((menu, index) => (
+
+                    <div onClick={() => setActiveIndex(index)} key={menu.id} className={`group flex items-center gap-2 w-full hover:bg-primary hover:text-white hover:rounded-md transition-all duration-200 ease-in-out hover:cursor-pointer ${activeIndex === index && 'bg-primary text-white'}`}>
+
 
                         <div className="ml-5">
                             {menu.icon}
