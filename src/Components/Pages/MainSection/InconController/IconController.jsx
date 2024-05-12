6d135013fb6d8a9ec3f97f98b3d728cccf22ec11
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-05-12 15:48:48 +0600

    add icon logo and name

--- src/Components/Pages/MainSection/InconController/IconController.jsx
+++ src/Components/Pages/MainSection/InconController/IconController.jsx
@@ -1,8 +1,18 @@
+import { Mouse } from "lucide-react";
+import { Slider } from "@/components/ui/slider"
+
 
 const IconController = () => {
     return (
         <div>
-icon
+
+            <div>
+                <label className="text-xl font-bold" htmlFor="mouse">Icon</label>
+                <div id="mouse" className="p-3 cursor-pointer bg-gray-200  rounded-md w-[50px] h-[50px] flex items-center my-1 ">
+                    <Mouse />
+                </div>
+            </div>
+
         </div>
     );
 };
