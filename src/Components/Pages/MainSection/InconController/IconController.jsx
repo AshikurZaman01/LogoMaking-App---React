7d64916eaab519437a8094fb7b881f903c9051e6
AshikurZaman01 <ashikurzaman774@gmail.com>
2024-05-12 16:19:44 +0600

    add range and set vlaue

--- src/Components/Pages/MainSection/InconController/IconController.jsx
+++ src/Components/Pages/MainSection/InconController/IconController.jsx
@@ -1,8 +1,16 @@
 import { Mouse } from "lucide-react";
-import { Slider } from "@/components/ui/slider"
+import { useState } from "react";
 
 
 const IconController = () => {
+
+    const [size, setSize] = useState(0);
+
+    const handleSize = (e) => {
+        setSize(e.target.value);
+        console.log(size);
+    }
+
     return (
         <div>
 
@@ -11,6 +19,15 @@ const IconController = () => {
                 <div id="mouse" className="p-3 cursor-pointer bg-gray-200  rounded-md w-[50px] h-[50px] flex items-center my-1 ">
                     <Mouse />
                 </div>
+            </div>
+
+            <div>
+                <label className="text-xl font-bold">Size</label>
+
+                <div id="mouse" className="p-3 cursor-pointer bg-gray-200  rounded-md  h-[50px] flex items-center my-1 ">
+                    <input onChange={handleSize} value={size} type="range" min={0} max={10} className="w-[80%]" />
+                </div>
+
             </div>
 
         </div>
